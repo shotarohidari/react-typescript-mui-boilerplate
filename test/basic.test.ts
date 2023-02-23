c016ec0a4c1aaa0196e623e439a7595128bf6929
左翔太朗 <zuoxiangtailang@sashoutarous-MacBook-Air.local>
2023-02-23 16:03:09 +0900

    細かな修正

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,10 +1,9 @@
-import {expect,describe,test} from "vitest";
+import { expect, describe, test } from 'vitest';
 
-const add = (num1:number,num2:number) => num1 + num2;
+const add = (num1: number, num2: number) => num1 + num2;
 
-describe("テストの書き方", () => {
-    test("1 + 1 = 2",() => {
-        expect(add(1,1)).toBe(2);
-    })
-})
-
+describe('テストの書き方', () => {
+  test('1 + 1 = 2', () => {
+    expect(add(1, 1)).toBe(2);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import {} from "vitest";
+import {} from 'vitest';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
-  }
+  },
 });
